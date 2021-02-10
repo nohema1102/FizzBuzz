c40facae78cb622cac0f665e8ff959921d8319b4
Nohemí <nohemi1180212345@gmail.com>
2021-02-10 11:18:45 -0700

    Se modifico FizzBuzz

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             int [] numerosfizz = new int [100];
+           int suma = 0;
+           for (int i = 0; i< numerosfizz.Length; i++)
+           {
+            numerosfizz [i] = i + 1; 
+            {
+             suma = suma + numerosfizz [i];
+            } 
+          
+           if ( numerosfizz [i] %3 == 0 && numerosfizz [i] %5 == 0)
+
+           {
+              Console.WriteLine ("FizzBuzz");
+           }
+
+           else if (numerosfizz [i] %3 == 0)
+           {
+               Console.WriteLine ("Fizz");
+           }
+           
+           else if (numerosfizz [i] %5 == 0)
+           {
+               Console.WriteLine ("Buzz");
+           }
+           
+           else 
+           {
+               Console.WriteLine (numerosfizz[i]);
+           }
         }
     }
+}
 }
